65778587e10f393d4249d9b5e4c8dc5ef2a7ed62
davina putri izzati gautama (2309116054) <144905548+davinasi@users.noreply.github.com>
2025-03-14 13:20:43 +0800

    Add files via upload

---
+++ webportofolio/main.js
@@ -0,0 +1,9 @@
+// main
+document.addEventListener('DOMContentLoaded', function(){
+    var typed = new Typed('.header-text h1', {
+        strings: ["Web-Develoment","Data Analytics","Digital Business Consultant"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+});
